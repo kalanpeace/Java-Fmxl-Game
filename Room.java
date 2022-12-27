29c2b2bc569fad12d18fbabb9aa215bae60450db
Kalan Peace <44334330+kalanpeace@users.noreply.github.com>
2022-12-27 01:32:19 -0500

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,38 @@
+package com.example.finalproject;
+
+public class Room {
+    private boolean NPC;
+    private boolean blocked;
+    private int gold;
+
+
+    public Room( boolean NPCMonster, boolean blocked, int gold) {
+        this.NPC = Math.random() < 0.5;
+        this.blocked = Math.random() < 0.5;
+        this.gold = (int)(Math.random() * 10);
+    }
+
+    public boolean isNPC() {
+        return NPC;
+    }
+
+    public void setNPC(boolean NPC) {
+        this.NPC = NPC;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+}
